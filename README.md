35d0f5c136f9b3c94d1eb1484b167edf10d74929
ZoneTwelve <zonetwelve159@gmail.com>
2023-04-19 13:34:40 +0800

    [add] installation instruction on install.cmd

---
+++ README.md
@@ -0,0 +1,12 @@
+# AT89S51 Development toolkit
+
+這是 AT89S51 相關的開發工具，基於實驗需求，因此將本工具整理並打包成 GitHub Repo。
+若相關軟體版權需要求下架，可在 issues 直接提出即可。
+
+## 使用說明
+
+{準備中...}
+
+## 環境
+
+OS: Windows 7
---
+++ install.cmd
@@ -0,0 +1,14 @@
+@echo off
+cls
+echo "請依循下列步驟進行"
+pause
+control printers
+echo 請選擇c51_pgm,並且按右鍵(內容)
+echo 硬體
+echo 變更設定
+echo 驅動程式
+echo 安裝驅動程式
+echo 選擇本地資料夾(LTC_USBDRV - 驅動)
+echo 安裝成功
+pause>nul
+pause>nul
